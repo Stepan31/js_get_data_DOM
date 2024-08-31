f92f8de8b6fd45d3f7ac43d69770e29b214e2301
Stepan Bahrynivskyi <bagrunivskiystepan@gmail.com>
2024-08-31 17:02:51 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const populationElements = document.querySelectorAll(".population");
+const populations = Array.from(populationElements).map((el)=>{
+    return parseInt(el.textContent.replace(/,/g, ""));
+});
+const totalPopulation = populations.reduce((sum, num)=>sum + num, 0);
+const averagePopulation = totalPopulation / populations.length;
+document.querySelector(".total-population").textContent = totalPopulation.toLocaleString("en-US");
+document.querySelector(".average-population").textContent = averagePopulation.toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
